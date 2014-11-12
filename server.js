a07d0a5e9cf38e3f547126777d8d76f869a3ced9
Hadi <hadichalabi@gmail.com>
2014-11-12 11:31:19 +0000

    tested and working server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs-layouts')
 app.use(express.static(__dirname + '/public'))
@@ -8,5 +10,7 @@ app.get ('/', function(req, res){
   res.render('index.ejs')
 });
 
-app.listen(3000);
+http.listen(3000);
 console.log('Listening to port 3000')
+
+module.exports = http;
---
+++ test/homepage_test.js
@@ -0,0 +1,32 @@
+var app = require('../server');
+var chai = require('chai');
+var expect = chai.expect;
+var Browser = require('zombie');
+
+
+describe('test', function() {
+
+  var browser
+
+  before(function() {
+    this.app = app.listen(3000);
+    browser = new Browser({ site: 'http://localhost:3000' });
+  });
+
+  beforeEach(function(done) {
+    browser.visit('/', done);
+  });
+
+  after(function(done) {
+    this.app.close(done)
+  });
+
+  it('visits the home page', function() {
+    expect(browser.success).to.be.true
+  });
+
+  it('says hello world I fucking hate you', function(){
+    expect(browser.text("h1")).to.eql("Hello world, I fucking hate you")
+  });
+
+});
